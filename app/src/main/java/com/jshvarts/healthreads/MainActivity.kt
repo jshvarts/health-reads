0e3bbb5ed93f1a545560940668815c39b4cddbcc
James Shvarts <jshvarts@gmail.com>
2020-08-31 22:30:43 -0400

    Add navigation library and view binding

--- app/src/main/java/com/jshvarts/healthreads/MainActivity.kt
+++ app/src/main/java/com/jshvarts/healthreads/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.jshvarts.healthreads
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.jshvarts.healthreads.databinding.ActivityMainBinding
+import com.jshvarts.healthreads.databinding.ActivityMainBinding.inflate
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = inflate(layoutInflater)
+    setContentView(binding.root)
+
+    val navController = findNavController(R.id.nav_host_fragment)
+    val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+    setSupportActionBar(binding.toolbar)
+
+    binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+    setupActionBarWithNavController(navController, appBarConfiguration)
   }
 }
---
+++ app/src/main/java/com/jshvarts/healthreads/booklist/BookListFragment.kt
@@ -0,0 +1,6 @@
+package com.jshvarts.healthreads.booklist
+
+import androidx.fragment.app.Fragment
+import com.jshvarts.healthreads.R
+
+class BookListFragment : Fragment(R.layout.fragment_book_list)
